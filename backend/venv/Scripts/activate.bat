c7dd4ce19f089029ba1e156528fb0655c9d91556
Estebom <estebomofficial@gmail.com>
2024-09-22 16:14:36 -0400

    Integrated LLM with backend.

--- backend/venv/Scripts/activate.bat
+++ backend/venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\mikes\Desktop\-\fall 2024\eng design 2 egn4952c\Professor PiBot\backend\venv
+set VIRTUAL_ENV=C:\Users\Esteban\Documents\GitHub\Professor-PiBot\backend\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
