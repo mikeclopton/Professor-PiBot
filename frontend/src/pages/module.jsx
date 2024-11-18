42fd3ed4745f0727c27aa7118938d26ae1d1f2cf
Robbie Salisbury <141463843+Papapepper1@users.noreply.github.com>
2024-11-17 21:24:02 -0500

    Merged newstyling  branch

--- frontend/src/pages/module.jsx
+++ frontend/src/pages/module.jsx
@@ -26,22 +26,24 @@ const ModuleSelection = () => {
   };
 
   return (
-    <div className="w-full mx-auto mt-16 p-8 bg-gray-900 rounded-lg text-center">
-      <h2 className="text-3xl font-semibold text-gray-100 mb-6">Select a Module</h2>
-      {error && <p className="text-red-500 mb-4">{error}</p>}
-      <ul className="list-none p-0">
-        {modules.map((module) => (
-          <li key={module} className="mb-4">
-            <button
-              className="w-full py-3 px-6 cursor-pointer transition-transform bg-gray-700 text-gray-100 rounded-lg border-b-4 border-gray-900 hover:bg-gray-500 hover:shadow-lg hover:-translate-y-1 active:shadow-none active:border-b-2 active:bg-gray-800 shadow-gray-900 shadow-md"
-              onClick={() => handleModuleSelect(module)}
-              disabled={loading}
-            >
-              {loading ? "Loading..." : `Module ${module}`}
-            </button>
-          </li>
-        ))}
-      </ul>
+    <div className="w-full flex justify-center mt-16">
+      <div className="inline-flex flex-col items-center p-8 bg-gray-800 rounded-lg text-center">
+        <h2 className="text-3xl font-semibold text-gray-100 mb-6">Select a Module</h2>
+        {error && <p className="text-red-500 mb-4">{error}</p>}
+        <ul className="list-none p-0 w-full">
+          {modules.map((module) => (
+            <li key={module} className="mb-4">
+              <button
+                className="w-64 py-3 px-6 cursor-pointer transition-transform bg-indigo-700 text-gray-100 rounded-lg border-b-4 border-indigo-900 hover:bg-indigo-500 hover:shadow-lg hover:-translate-y-1 active:shadow-none active:border-b-2 active:bg-indigo-800 shadow-indigo-900 shadow-md"
+                onClick={() => handleModuleSelect(module)}
+                disabled={loading}
+              >
+                {loading ? "Loading..." : `Module ${module}`}
+              </button>
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 };
